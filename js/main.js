9a9f9183960f296a17768f423119ad087542ed37
unknown <mardonbekabdullaxayev@gmail.com>
2023-07-13 11:06:05 +0300

    buildstore

---
+++ js/main.js
@@ -0,0 +1,76 @@
+let bgone = document.querySelector(".button-the-redemp-one")
+let bgtwo = document.querySelector(".button-the-redemp-two")
+let bgthree = document.querySelector(".button-the-redemp-three")
+let bgfour = document.querySelector(".button-the-redemp-four")
+let bgfive = document.querySelector(".button-the-redemp-five")
+let bgsix = document.querySelector(".button-the-redemp-six")
+let bgseven = document.querySelector(".button-the-redemp-seven")
+let bgeight = document.querySelector(".button-the-redemp-eight")
+let bgnine = document.querySelector(".button-the-redemp-nine")
+let bgten = document.querySelector(".button-the-redemp-ten")
+let backfoto = document.querySelector(".card-back-room")
+
+
+
+let textbox = document.querySelector(".text-card-mini")
+let boxbigtext = document.querySelector(".text-card-grey")
+
+function clickone() {
+    boxbigtext.innerHTML = "Разработка проектной документации"
+    textbox.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! "
+    backfoto.style.backgroundImage = "url(./assets/img/side-view-1.png)"
+}
+
+function clicktwo() {
+    boxbigtext.innerHTML = " Разрешения на строительство"
+    textbox.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! "
+    backfoto.style.backgroundImage = "url(./assets/img/side-view-2.png)"
+}
+
+function clickthree() {
+    boxbigtext.innerHTML = "Производство металлоконструкций"
+    textbox.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! "
+    backfoto.style.backgroundImage = "url(./assets/img/side-view-3.png)"
+}
+
+function clickfour() {
+    boxbigtext.innerHTML = "Планировка земельного участка"
+    textbox.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! "
+    backfoto.style.backgroundImage = "url(./assets/img/side-view-4.png)"
+}
+
+function clickfive() {
+    boxbigtext.innerHTML = "Фундаментные работы"
+    textbox.innerHTML = "Фундаментные работы – комплекс мероприятий, необходимых для возведения надежного основания под здания. Они включают в себя рытье траншеи, создание строительного «пирога» против усадки, армирование, бетонирование и иные меры. Специалисты нашей компании проводят фундаментные работы с соблюдением СНиПов и ГОСТов, поэтому основания, возводимые нами, отличаются надежностью, долговечностью и прочностью.Требования к основанию.Фундамент – несущая конструкция, которая повышает срок эксплуатации зданий и любых других сооружений. Он берет на себя основные нагрузки, то есть вес постройки, равномерно распределяя его по всей площади. Наиболее часто фундаментную конструкцию изготавливают из бетона, поскольку именно этот материал обладает максимальной долговечностью и прочностью. Специалисты нашей компании возводят основания под постройки с учетом строительных стандартов и правил. "
+    backfoto.style.backgroundImage = "url(./assets/img/side-view-5.png)"
+}
+
+function clicksix() {
+    boxbigtext.innerHTML = "Монтаж металлоконструкций"
+    textbox.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! "
+    backfoto.style.backgroundImage = "url(./assets/img/side-view-6.png)"
+}
+
+function clickseven() {
+    boxbigtext.innerHTML = "Монтажсэндвич-панелей"
+    textbox.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! "
+    backfoto.style.backgroundImage = "url(./assets/img/side-view-7.png)"
+}
+
+function clickeight() {
+    boxbigtext.innerHTML = "Монтаж инженерных сетей"
+    textbox.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! "
+    backfoto.style.backgroundImage = "url(./assets/img/side-view-8.png)"
+}
+
+function clicknine() {
+    boxbigtext.innerHTML = "Благоустройство территории"
+    textbox.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! "
+    backfoto.style.backgroundImage = "url(./assets/img/side-view-9.png)"
+}
+
+function clickten() {
+    boxbigtext.innerHTML = "Получения ввода в эксплуатацию"
+    textbox.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minima numquam minus dolor veritatis, nihil labore laborum molestias ex, cupiditate vero. Delectus minima amet a id. Consequatur rerum aperiam ad sed nesciunt nisi quos repudiandae eum. Eum quam iste praesentium suscipit facere, nihil dolorum ad explicabo accusantium et nisi. Sint! "
+    backfoto.style.backgroundImage = "url(./assets/img/side-view-10.png)"
+}
